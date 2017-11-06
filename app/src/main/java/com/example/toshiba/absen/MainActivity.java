cf96db00ed34f18494a156c5d01944aab438cf1d
gandhybagoes <gbagoescahyono@gmail.com>
2017-11-06 17:05:05 +0700

    commit 2

--- app/src/main/java/com/example/toshiba/absen/MainActivity.java
+++ app/src/main/java/com/example/toshiba/absen/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.toshiba.absen;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String webAddress = "https://temubarang.000webhostapp.com";
+    private WebView webView;
+    private ProgressBar progressBar;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frameLayout);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new HelpClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                frameLayout.setVisibility(View.VISIBLE);
+                progressBar.setProgress(progress);
+
+                setTitle("Loading...");
+
+                if (progress == 100) {
+                    frameLayout.setVisibility(View.GONE);
+                    setTitle(view.getTitle());
+                }
+                super.onProgressChanged(view, progress);
+            }
+        });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl(webAddress);
+        progressBar.setProgress(0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class HelpClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            frameLayout.setVisibility(View.VISIBLE);
+            return true;
+        }
     }
 }
